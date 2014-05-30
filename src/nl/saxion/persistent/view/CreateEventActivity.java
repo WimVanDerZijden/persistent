592871a9e636f3499dbad0891fa211ad8a5e6b82
Erik Sijssens <eriksijssens@gmail.com>
2014-05-30 16:15:38 +0200

    Added ui to create event activity

--- src/nl/saxion/persistent/view/CreateEventActivity.java
+++ src/nl/saxion/persistent/view/CreateEventActivity.java
@@ -1,8 +1,17 @@
 package nl.saxion.persistent.view;
 
+import java.util.Calendar;
+
 import nl.saxion.persistent.R;
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.DatePicker;
+import android.widget.Spinner;
 
 public class CreateEventActivity extends Activity {
 
@@ -10,5 +19,6 @@ public class CreateEventActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_event);
+		
 	}
 }
